d8fd4c23c90a16dea012f8d85b889ea4c16c819b
Prashanth Selvam <prashanth.selvam@gmail.com>
2024-08-09 15:35:48 -0400

    Cleanup

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -66,16 +66,6 @@ const TriggerCitiesMap: React.FC<TTriggerCitiesMapProps> = ({ cities }) => {
               maxPopulation={maxPopulation}
             />
           ))}
-          {/* <Popup
-             longitude={popupCity.CITY_LNG}
-             latitude={popupCity.CITY_LAT}
-             onClose={() => setPopupCity(null)}
-             closeButton={false}
-             style={{ background: 'red' }}
-             offset={24}
-           >
-             <PopupContent city={popupCity} />
-           </Popup> */}
           <NavigationControl position="top-right" showZoom={true} />
         </Map>
         <div id="legend">
@@ -259,51 +249,5 @@ const PopupContent: React.FC<TPopupContentProps> = ({ city, handleClose, isPopup
     </AnimatePresence>
   );
 };
-
-// const PopupContent: React.FC<TPopupContentProps> = ({ city }) => {
-//   return (
-//     <div className="flex flex-row bg-zinc-950 border border-gray-800 rounded-sm w-[644px] shadow-lg">
-//       <div className="w-1/2">
-//         <div className="bg-gray-300 w-full h-36 p-6">
-//           <div className="p-2 bg-black text-gray-100 w-fit rounded-lg font-semibold">
-//             {city.CONTINENT.toUpperCase()}
-//           </div>
-//         </div>
-//         <div className="px-10 pt-4 pb-10 w-full">
-//           <div className="flex flex-row justify-between items-center border-b border-white mb-6">
-//             <h4 className="text-xl font-bold">{city.CITY_NAME}</h4>
-//             <p className="text-lg">{city.COUNTRY_NAME}</p>
-//           </div>
-//           <p>
-//             Chicago is the most populous city in the U.S. state of Illinois and in the Midwestern United States. Located
-//             on the shore of Lake Michigan, Chicago was incorporated as a city in 1837 near a portage between the Great
-//             Lakes and the Mississippi River watershed. Chicago is the most populous city in the U.S. state of Illinois
-//             and in the Midwestern United States.
-//           </p>
-//         </div>
-//       </div>
-//       <div className="w-1/2 flex flex-col px-10 py-6 bg-gray-950">
-//         <div className="text-center rounded-lg border border-white p-2 font-semibold">
-//           {city.TRIGGER_CITY_TIER.toUpperCase()}
-//         </div>
-//         <div className="mt-4">
-//           <p className="border-b border-gray-300 pb-2 text-lg font-bold">Population</p>
-//           <p className="text-lg pt-2">{city.CITY_POPULATION.toLocaleString()}</p>
-//         </div>
-//         <div className="mt-6">
-//           <p className="border-b border-gray-300 pb-2 text-lg font-bold mb-2">Top 5 Genres</p>
-//           {city.CITY_TOP_5_GENRES.map((genre, i) => {
-//             return (
-//               <p key={genre} className="py-2 text-lg border-b border-gray-600">
-//                 {genre}
-//                 <span className="float-right font-bold text-gray-600">{i}</span>
-//               </p>
-//             );
-//           })}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 
 export default TriggerCitiesMap;
